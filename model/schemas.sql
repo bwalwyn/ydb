30bb7c8915aae70d89155bf6645eb4146e4696ab
bwalwyn <bwalwyn@gmail.com>
2019-08-23 21:18:52 +0200

    setup

---
+++ model/schemas.sql
@@ -0,0 +1,14 @@
+-- -----------------------------------------------------
+-- Schema
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS Zoho ;
+USE Zoho ;
+
+CREATE SCHEMA IF NOT EXISTS Mobile ;
+USE Mobile ;
+
+CREATE SCHEMA IF NOT EXISTS Finance ;
+USE Finance ;
+
+CREATE SCHEMA IF NOT EXISTS Sandbox ;
+USE Sandbox ;
